0fb72b2409ce3724f33a75b28522071f67e1acf2
Amit Bhadoria <gnulib@gmail.com>
2016-05-26 22:03:23 -0700

    Cleaned up resource paths for authentication filter
    fixed incorrect use of RBAC filter

--- src/main/java/com/integratingfactor/idp/account/api/ping/Hello.java
+++ src/main/java/com/integratingfactor/idp/account/api/ping/Hello.java
@@ -7,23 +7,26 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.integratingfactor.idp.lib.client.filter.IdpApiAuthFilter;
+import com.integratingfactor.idp.lib.client.rbac.IdpApiRbacDetails;
 import com.integratingfactor.idp.lib.client.rbac.IdpRbacPolicy;
 
 @RestController
 public class Hello {
     private static Logger LOG = Logger.getLogger(Hello.class.getName());
 
-    @RequestMapping(value = { "/ping", "/api/ping" })
+    @RequestMapping(value = { "/ping" })
     public Pong ping(HttpServletRequest request) {
         LOG.info("Ping request from " + request.getRemoteAddr());
         return new Pong("Hello World!");
     }
 
-    @RequestMapping(value = { "/api/v1/ping" })
-    @IdpRbacPolicy(roles = "USER")
+    @RequestMapping(value = { "/v1/ping" })
+    @IdpRbacPolicy()
     public Pong securedPing(HttpServletRequest request) {
         LOG.info("Ping request from " + request.getRemoteAddr());
-        return new Pong("Hello World!");
+        IdpApiRbacDetails details = IdpApiAuthFilter.getRbacDetails();
+        return new Pong("Hello " + details.getAccountId() + "!");
     }
 
     public static class Pong {

705bee64e2266fc795d6611913b284597bff8b5a
Amit Bhadoria <gnulib@gmail.com>
2016-05-26 21:37:37 -0700

    added RBAC filter and extended ping API to test RBAC

--- src/main/java/com/integratingfactor/idp/account/api/ping/Hello.java
+++ src/main/java/com/integratingfactor/idp/account/api/ping/Hello.java
@@ -7,12 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.integratingfactor.idp.lib.client.rbac.IdpRbacPolicy;
+
 @RestController
 public class Hello {
     private static Logger LOG = Logger.getLogger(Hello.class.getName());
 
     @RequestMapping(value = { "/ping", "/api/ping" })
     public Pong ping(HttpServletRequest request) {
+        LOG.info("Ping request from " + request.getRemoteAddr());
+        return new Pong("Hello World!");
+    }
+
+    @RequestMapping(value = { "/api/v1/ping" })
+    @IdpRbacPolicy(roles = "USER")
+    public Pong securedPing(HttpServletRequest request) {
         LOG.info("Ping request from " + request.getRemoteAddr());
         return new Pong("Hello World!");
     }

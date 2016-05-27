bc7c88ea4bf7fd04dce2452f3258085b830a2933
Amit Bhadoria <gnulib@gmail.com>
2016-05-26 19:29:34 -0700

    simple stateless microservice with ping API

---
+++ src/main/java/com/integratingfactor/idp/account/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.integratingfactor.idp.account.config;
+
+import java.util.logging.Logger;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    private static Logger LOG = Logger.getLogger(AppConfig.class.getName());
+
+}
